b12991c4fd57e792724664dd0dcde763cab058df
Niru Maheswaranathan <nirum@stanford.edu>
2015-08-14 04:18:46 -0700

    WIP generating custom objective

--- mello/core.py
+++ mello/core.py
@@ -29,7 +29,13 @@ class Experiment(object):
 
         """
 
-        self.stimulus_data = stim
+        assert type(stim) == np.ndarray and stim.ndim == 3, \
+            "Stimulus must be a 3 dimensionsal (space x space x time) array"
+
+        assert type(time) == np.ndarray and time.ndim == 1, \
+            "Time vector must be an one dimensional numpy array"
+
+        self.stim = stim
         self.cells = cells
         self.time = time
         self.dt = dt
@@ -37,7 +43,7 @@ class Experiment(object):
         spikes = list()
         for cell in self.cells:
             spikes.append(np.append(0, binspikes(cell, time=time)[0]))
-        self.spikes_data = np.vstack(spikes)
+        self.spikes = np.vstack(spikes)
 
     def __len__(self):
         return len(self.time)
@@ -46,28 +52,26 @@ class Experiment(object):
     def tmax(self):
         return self.time[-1]
 
-    def stim(self, history=0):
-        """Return a view into the stimulus"""
-        return rolling_window(self.stimulus_data, history)
+    def stim_sliced(self, history):
+        """Returns a view into the stimulus array"""
+        return rolling_window(self.stim, history)
 
-    def spikes(self, history=0):
-        """Return a view into the spikes array"""
-        return rolling_window(self.spikes_data, history)
-
-    def __call__(self, t, stim_hist=50, spk_hist=80):
-        st = self.stimulus[..., (t-stim_hist):t]
-        spk = self.spikes[..., (t-spk_hist-1):(t-1)]
-        return {'stim': st, 'spk': spk}
+    def spikes_sliced(self, history):
+        """Returns a view into the spikes array"""
+        return rolling_window(self.spikes, history)
 
     def ste(self, stim_hist, ci):
-        return (self.stimulus[..., (t-stim_hist):t].astype('float')
+        return (self.stim[..., (t-stim_hist):t].astype('float')
                 for t in range(len(self))
                 if self.spikes[ci,t] > 0 and t >= stim_hist)
 
     def features(self, stim_hist=50, spk_hist=80):
+        """Note: Slower than using np.einsum"""
         mint = max(stim_hist, spk_hist+1)
         for t in range(mint, len(self)):
-            yield self(t, stim_hist, spk_hist)
+            st = self.stim[..., (t-stim_hist):t]
+            spk = self.spikes[..., (t-spk_hist-1):(t-1)]
+            yield {'stim': st, 'spk': spk}
         raise StopIteration
 
 class Objective(object):
@@ -75,22 +79,23 @@ class Objective(object):
     Experiment agnostic
     """
 
-    def __init__(self, get_features, num_features, dt):
-        self.features = get_features
-        self.N = float(num_features)
-        self.dt = dt
+    def __init__(self):
+        pass
+        # self.features = get_features
+        # self.N = float(num_features)
+        # self.dt = dt
 
-        # compute average feature
-        for idx, f in enumerate(self.features()):
+        # # compute average feature
+        # for idx, f in enumerate(self.features()):
 
-            if idx == 0:
-                tmp = f
+            # if idx == 0:
+                # tmp = f
 
-            else:
-                for k, v in f.items():
-                    tmp[k] += v
+            # else:
+                # for k, v in f.items():
+                    # tmp[k] += v
 
-        self.average_feature = valmap(lambda v: np.divide(v, N), tmp)
+        # self.average_feature = valmap(lambda v: np.divide(v, N), tmp)
 
     def project(self, x, theta):
         return np.sum([np.tensordot(theta[k], x[k]) for k in theta.keys()])
@@ -107,6 +112,9 @@ class Objective(object):
         TODO
 
         """
+
+        # np.einsum('ijtk,ijk->t', stim, theta) # 3
+        # np.einsum('itk,ik->t', stim, theta) # 2
 
         avg_proj = self.project(self.average_feature, theta)
 
@@ -128,6 +136,32 @@ class Objective(object):
         grad = itemmap(lambda k, v: (self.dt / self.N) * v - avg_proj[k], weighted_features)
 
         return obj, grad
+
+
+def make_objective(ex, ci, stim_hist=40, spk_hist=80):
+
+    mint = np.max(stim_hist, spk_hist+1)
+    T = float(len(ex) - mint)
+
+    stim_sliced = ex.stim_sliced(stim_hist)
+    spk_sliced = ex.spikes_sliced(spk_hist+1)
+
+    nspk = ex.spikes[ci]
+
+    stim_avg = np.einsum('ijtk,t->ijk', stim_sliced, nspk[stim_hist:]) / float(len(ex) - stim_hist)
+    spk_avg = np.einsum('itj,t->ij', spk_sliced, nspk[spk_hist:])[:,:-1] / float(len(ex) - spk_hist + 1)
+
+    def f_df(theta):
+
+        ustim = np.einsum('ijtk,ijk->t', stim_sliced, theta['stim'])
+        uspk = np.einsum('itj,ij->t', spk_sliced, np.hstack((theta['spk'], np.zeros((theta['spk'].shape[0],1)))))
+        rhat = np.exp(ustim + uspk)
+
+        obj = (ex.dt / float(rhat.size)) * np.sum(rhat) - np.dot(theta['stim'].ravel(), stim_avg.ravel()) - np.dot(theta['spk'].ravel(), spk_avg.ravel())
+        grad_stim = (ex.dt / T) * np.einsum('ijtk,t->ijk', stim_sliced, rhat)
+
+    return f_df
+
 
 # USAGE:
 # ex = Experiment(stim, time, spikes, dt)
@@ -165,16 +199,10 @@ def rolling_window(array, window):
            [ 6.,  7.,  8.]])
 
     """
-
-    assert window >= 0, "`window` must be at least 1."
+    assert window >= 1, "`window` must be at least 1."
     assert window < array.shape[-1], "`window` is too long."
 
-    if window == 0:
-        # without strides
-        return array
-
-    else:
-        # with strides
-        shape = array.shape[:-1] + (array.shape[-1] - window, window)
-        strides = array.strides + (array.strides[-1],)
-        return np.lib.stride_tricks.as_strided(array, shape=shape, strides=strides)
+    # # with strides
+    shape = array.shape[:-1] + (array.shape[-1] - window, window)
+    strides = array.strides + (array.strides[-1],)
+    return np.lib.stride_tricks.as_strided(array, shape=shape, strides=strides)
